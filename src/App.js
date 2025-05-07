8378363cc43102f5deb49c78c9f26b0142dbee76
Rashmi-kavindya <kavidissanayaka20@gmail.com>
2025-05-07 09:19:55 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LoginPage from './Components/LoginPage/LoginPage';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ width: '100%', maxWidth: '100%', margin: 0, padding: 0 }}>
       <LoginPage />
     </div>
   );
